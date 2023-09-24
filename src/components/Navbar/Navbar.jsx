1db7981abd7a6c3dc1682aa51aad8367b0d4bf53
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-09-24 16:26:14 -0400

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import Styles from './Navbar.module.css'
+
+export default function Navbar() {
+  return (
+    <div className={Styles.box}>
+        <p>Logo</p>
+        <p>Option1 Option2 Option3 Option4</p>
+        <p>Login</p>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
