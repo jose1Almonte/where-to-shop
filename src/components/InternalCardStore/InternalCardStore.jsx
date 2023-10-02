78ac1d089eb658dbe79ab4deaacf4d78a554e03e
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-10-02 11:33:29 -0400

    progressBar

--- src/components/InternalCardStore/InternalCardStore.jsx
+++ src/components/InternalCardStore/InternalCardStore.jsx
@@ -1,11 +1,24 @@
 import Styles from './InternalCardStore.module.css'
 // import todoQueso from '../../images/todoQuesoFront.jpg'
+import ProgressBar from "@ramonak/react-progress-bar";
 
 export default function InternalCardStore() {
   return (
     <div className={Styles.container}>
         <div className={Styles.image} style={{backgroundImage: 'url(src/images/todoQuesoFront.jpg)'}}/>
         <h2 className={Styles.title}>Food - All Cheese</h2>
+        
+        <ProgressBar 
+        completed={60}
+        // barContainerClassName={Styles.progressBarContainer}
+        borderRadius= {3}
+        height='5px'
+        labelSize= {10}
+        bgColor = {'#00A991'} // progressed bar
+        baseBgColor = {'#E0E0E0'} // all bar
+        isLabelVisible={false}
+        animateOnRender={true}
+        />
     </div>
   )
 }
