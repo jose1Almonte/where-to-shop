6f40f62b657af7bbab8a91cf0ca76038ae74038f
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-10-13 08:33:28 -0400

    host

--- src/components/SelectionType/SelectionType.jsx
+++ src/components/SelectionType/SelectionType.jsx
@@ -4,13 +4,13 @@ export default function SelectionType({selected, store}) {
 
     switch(selected) {
         case 'Stores':       
-            return <img src={store.Stores} alt="" style={{width: '100%'}} />;
+            return <img src={store.Stores} alt="" style={{width: '100%'}} loading='eager'/>;
         case 'Food':
-            return <img src={store.Food} alt="" style={{width: '100%'}} />;
+            return <img src={store.Food} alt="" style={{width: '100%'}} loading='eager'/>;
         case 'Drinks':
-            return <img src={store.Drinks} alt="" style={{width: '100%'}} />;
+            return <img src={store.Drinks} alt="" style={{width: '100%'}} loading='eager'/>;
         case 'Snacks':
-            return <img src={store.Snacks} alt="" style={{width: '100%'}} />;
+            return <img src={store.Snacks} alt="" style={{width: '100%'}} loading='eager'/>;
     }
 }
 
