d39bcc2505ea4aa44958e9370585a32cc8a866ff
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-11-25 11:44:45 -0400

    interactive number or units of product

--- src/components/ProductQuantitySelection/ProductQuantitySelection.jsx
+++ src/components/ProductQuantitySelection/ProductQuantitySelection.jsx
@@ -2,12 +2,27 @@ import PlusOrMinusButton from '../PlusOrMinusButton/PlusOrMinusButton'
 import Styles from './ProductQuantitySelection.module.css'
 import plus from '../../vectors/plus.svg'
 import minus from '../../vectors/minus.svg'
+import { useState } from 'react';
 
-export default function ProductQuantitySelection() { 
-    return (
+export default function ProductQuantitySelection() {
+  const [quantity, setQuantity] = useState('0');
+
+  const handleInputChange = (event) => {
+    console.log('event', event.target.value)
+    setQuantity(event.target.value);
+  };
+
+  return (
     <div className={Styles.container}>
         <PlusOrMinusButton vector={minus}/>
-        <input type="text" className={Styles.value} placeholder='0' />
+        <input
+        type="text"
+        className={Styles.value}
+        placeholder="0"
+        value={quantity}
+        onChange={handleInputChange}
+        style={{ width: `${quantity.length * 2.2}vw` }}
+      />
         <PlusOrMinusButton vector={plus}/>
     </div>
   )
